376404d553299dd09c9b577eb0c340e99638bb29
rsambouncer <rsambouncer@gmail.com>
2017-10-29 15:00:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,25 +15,20 @@ const server = express()
 const wss = new SocketServer({ server });
 const users = [];
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  users[ws.id] = {name:"anonymous"};
-  ws.on('message',(message) => {
-    users[ws.id]['name'] = message;
-    ws.send('00:'+users[ws.id]['name']);
-  });
-  ws.on('close', () => {
-    users.splice(ws.id,1);
-    console.log('Client disconnected');
-    wss.clients.forEach((client) => client.send('01:'+wss.clients.length));
-  });
-  ws.send('00:'+users[ws.id]['name']);
-  ws.send('02:'+new Date().toTimeString());
-  wss.clients.forEach((client) => client.send('01:'+wss.clients.length));
-});
 
+
+
+var testaaa = require('backend/game.js').exportFunction();
+wss.on('connection', (ws) => {
+  users[ws.id] = {name:"anonymous"};
+  ws.on('message',(message) => {});
+  ws.on('close', ()=>users.splice(ws.id,1) );
+  ws.send(""+testaaa);
+});
+/*
 setInterval(() => {
   wss.clients.forEach((client) => {
     client.send('02:'+new Date().toTimeString());
   });
 }, 1000);
+*/
