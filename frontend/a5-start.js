21891a70f4068e79c092d03dee7f92642e6f231c
rsambouncer <rsambouncer@gmail.com>
2017-11-01 11:07:20 -0700

    Update a5-start.js

--- frontend/a5-start.js
+++ frontend/a5-start.js
@@ -9,8 +9,9 @@ var UPDATEALL = function() {
         }
     }
     
+    drawScene();
+    
 };
 
 
 window.setTimeout(function(){window.setInterval(UPDATEALL,15);}, 500);
-window.setTimeout(function(){window.setInterval(drawScene,30);}, 500);
