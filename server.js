89be4937d745bec980f0094f696e067cb0bcbb29
rsambouncer <rsambouncer@gmail.com>
2017-11-01 11:16:10 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const server = express()
 const wss = new SocketServer({ server });
 const peoples = [];
 const testaaa = require('./backend/game.js').exportFunction();
-const pppperson = require(./backend/player.js).exportFunction;
+//const pppperson = require(./backend/player.js).exportFunction;
 
 
 wss.on('connection', (ws) => {
