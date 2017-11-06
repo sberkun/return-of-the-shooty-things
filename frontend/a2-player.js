e68613c7f737e9258bc0f1d3df43e5a95ad08940
rsambouncer <rsambouncer@gmail.com>
2017-11-06 11:32:52 -0800

    Update a2-player.js

--- frontend/a2-player.js
+++ frontend/a2-player.js
@@ -33,69 +33,71 @@ function player(){
       this.turret2={
           //add stuff later, defence weapon............................................................ 
       };
-      this.turret1.fire = function(fff153vvb){
-          if(ply.controls.fireready){if(fff153vvb){
-            for(var a=ply.turret1.numshots;a>0;a--){
-              bullets.push(new bullet(
-                  ply.x-ply.turret1.shaftlength*(Math.sin(ply.rt)),
-                  ply.y-ply.turret1.shaftlength*(Math.cos(ply.rt)),
-                  ply.turret1.bulletspeed,
-                  (((ply.rt+(Math.random()-0.5)*ply.turret1.accuracy)
-                    %(2*Math.PI))+(2*Math.PI))%(2*Math.PI),
-                  100,10
-              ));
-              ply.hull.t--;
-            }
-            fireready = false;
-          }}
-          else if(firetime++>ply.turret1.reloadtime){fireready = true;firetime=0;}
-      };
-      this.die = function(){
-      };
-      this.updateCanDieYet = function(){
-          if(ply.hull.t<0) return true;
-          if(ply.hull.t<ply.hull.st) ply.hull.t+=ply.hull.regen;
-  
-          var tttvx = 0;
-          var tttvy = 0;
-    
-          if(mouse_control){
-            if(ply.controls.www){tttvx-=ply.speed*Math.sin(ply.rt);tttvy-=ply.speed*Math.cos(ply.rt);}
-            if(ply.controls.sss){tttvx+=ply.speed*Math.sin(ply.rt);tttvy+=ply.speed*Math.cos(ply.rt);}
-            if(ply.controls.aaa){tttvy+=ply.speed*Math.sin(ply.rt);tttvx-=ply.speed*Math.cos(ply.rt);}
-            if(ply.controls.ddd){tttvy-=ply.speed*Math.sin(ply.rt);tttvx+=ply.speed*Math.cos(ply.rt);}
-            if(true){ply.turret1.fire(ply.controls.cuc);}
-            if(true){ply.turret2.fire(ply.controls.cdc);}
-            if(ply.controls.clc){ply.rt+=ply.rspeed;}
-            if(ply.controls.crc){ply.rt-=ply.rspeed;}
-          }else{
-            if(ply.controls.cuc){tttvx-=ply.speed*Math.sin(ply.rt);tttvy-=ply.speed*Math.cos(ply.rt);}
-            if(ply.controls.cdc){tttvx+=ply.speed*Math.sin(ply.rt);tttvy+=ply.speed*Math.cos(ply.rt);}
-            if(ply.controls.clc){tttvy+=ply.speed*Math.sin(ply.rt);tttvx-=ply.speed*Math.cos(ply.rt);}
-            if(ply.controls.crc){tttvy-=ply.speed*Math.sin(ply.rt);tttvx+=ply.speed*Math.cos(ply.rt);}
-            if(true){ply.turret1.fire(ply.controls.www);}
-            if(true){ply.turret2.fire(ply.controls.sss);}
-            if(ply.controls.aaa){ply.rt+=ply.rspeed;}
-            if(ply.controls.ddd){ply.rt-=ply.rspeed;}
-          }
-  
-          if(tttvx*tttvx+tttvy*tttvy-ply.speed*ply.speed>0.005){tttvx*=0.707106781;tttvy*=0.707106781;}
-          ply.vx+=tttvx;
-          ply.vy+=tttvy;
-          ply.x+=ply.vx;
-          ply.y+=ply.vy;
-          
-          ply.vx = ply.vx*ply.getFriction;
-          ply.vy = ply.vy*ply.getFriction;
-          if(Math.abs(ply.vx)<0.005) ply.vx = 0;
-          if(Math.abs(ply.vy)<0.005) ply.vy = 0;
-      };
 }
 
 
-
-
-
+var fireTurret1 = function(theply, fff153vvb){
+    if(theply.controls.fireready){if(fff153vvb){
+      for(var a=theply.turret1.numshots;a>0;a--){
+        bullets.push(new bullet(
+            theply.x-theply.turret1.shaftlength*(Math.sin(theply.rt)),
+            theply.y-theply.turret1.shaftlength*(Math.cos(theply.rt)),
+            theply.turret1.bulletspeed,
+            (((theply.rt+(Math.random()-0.5)*theply.turret1.accuracy)
+              %(2*Math.PI))+(2*Math.PI))%(2*Math.PI),
+            100,10
+        ));
+        theply.hull.t--;
+      }
+      theply.controls.fireready = false;
+    }}
+    else if(theply.controls.firetime++>theply.turret1.reloadtime)
+      {theply.controls.fireready = true;theply.controls.firetime=0;}
+};
+var fireTurret2 = function(theply, fff153vvb){
+};
+var diePlayer = function(theply){
+};
+var drawPlayer = function(theply){
+};
+var updateCanDieYetPlayer = function(theply){
+    if(theply.hull.t<0) return true;
+    if(theply.hull.t<theply.hull.st) theply.hull.t+=theply.hull.regen;
+  
+    var tttvx = 0;
+    var tttvy = 0;
+    
+    if(mouse_control){
+      if(theply.controls.www){tttvx-=theply.speed*Math.sin(theply.rt);tttvy-=theply.speed*Math.cos(theply.rt);}
+      if(theply.controls.sss){tttvx+=theply.speed*Math.sin(theply.rt);tttvy+=theply.speed*Math.cos(theply.rt);}
+      if(theply.controls.aaa){tttvy+=theply.speed*Math.sin(theply.rt);tttvx-=theply.speed*Math.cos(theply.rt);}
+      if(theply.controls.ddd){tttvy-=theply.speed*Math.sin(theply.rt);tttvx+=theply.speed*Math.cos(theply.rt);}
+      if(true){theply.turret1.fire(theply.controls.cuc);}
+      if(true){theply.turret2.fire(theply.controls.cdc);}
+      if(theply.controls.clc){theply.rt+=theply.rspeed;}
+      if(theply.controls.crc){theply.rt-=theply.rspeed;}
+    }else{
+      if(theply.controls.cuc){tttvx-=theply.speed*Math.sin(theply.rt);tttvy-=theply.speed*Math.cos(theply.rt);}
+      if(theply.controls.cdc){tttvx+=theply.speed*Math.sin(theply.rt);tttvy+=theply.speed*Math.cos(theply.rt);}
+      if(theply.controls.clc){tttvy+=theply.speed*Math.sin(theply.rt);tttvx-=theply.speed*Math.cos(theply.rt);}
+      if(theply.controls.crc){tttvy-=theply.speed*Math.sin(theply.rt);tttvx+=theply.speed*Math.cos(theply.rt);}
+      if(true){theply.turret1.fire(theply.controls.www);}
+      if(true){theply.turret2.fire(theply.controls.sss);}
+      if(theply.controls.aaa){theply.rt+=theply.rspeed;}
+      if(theply.controls.ddd){theply.rt-=theply.rspeed;}
+    }
+  
+    if(tttvx*tttvx+tttvy*tttvy-theply.speed*theply.speed>0.005){tttvx*=0.707106781;tttvy*=0.707106781;}
+    theply.vx+=tttvx;
+    theply.vy+=tttvy;
+    theply.x+=theply.vx;
+    theply.y+=theply.vy;
+          
+    theply.vx = theply.vx*theply.getFriction;
+    theply.vy = theply.vy*theply.getFriction;
+    if(Math.abs(theply.vx)<0.005) theply.vx = 0;
+    if(Math.abs(theply.vy)<0.005) theply.vy = 0;
+};
 
 
 
@@ -175,7 +177,7 @@ ply.turret2.fire = function(fff){
 ply.die = function(){
   document.body.innerHTML = "ur bad kid";
 };
-var updateCanDieYetPlayer = function(theply){
+updateCanDieYetPlayer = function(theply){
   if(theply.hull.t<0) return true;
   if(theply.hull.t<theply.hull.st) theply.hull.t+=theply.hull.regen;
   
