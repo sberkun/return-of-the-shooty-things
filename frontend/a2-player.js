3de6d6da8a013f92b54ca7569250b792db51318e
rsambouncer <rsambouncer@gmail.com>
2018-01-22 11:51:55 -0800

    Update a2-player.js

--- frontend/a2-player.js
+++ frontend/a2-player.js
@@ -2,22 +2,12 @@ function player(){
       this.vx=0;this.vy=0;
       this.friction=0.9;this.getFriction=0.9;
       this.x=0;this.y=0;
-      this.rt=0;this.speed=0.4;this.rspeed=2*Math.PI/180;
-      this.d=64; //diameter
+      this.rt=0;this.speed=0.4;
+      this.d=64; //diameter of circular hitbox
       this.hull={
         st: 50000,
         t:  50000,
         regen: 200,
-      };
-      this.turret1={
-          numshots:5,
-          accuracy:0.6,
-          shaftlength:64,
-          reloadtime:0,
-          bulletspeed:6
-      };
-      this.turret2={
-          //add stuff later, defence weapon............................................................ 
       };
 }
 
@@ -29,6 +19,7 @@ var ply = new player();
         mouseY:0,
         mouse_control: true,
         turret_control: true,
+        rspeed:2*Math.PI/180,
         clm:false, crm:false, mousehm:0,
         www:false, aaa:false, sss:false, ddd:false,
         zzz:false, xxx:false, cmc:false, cpc:false,
@@ -39,32 +30,43 @@ var ply = new player();
         fireready: true,
         firetime: 0,
       };
-      ply.fireTurret1 = function(fff153vvb){
-            
-      };
-      ply.fireTurret2 = function(fff153vvb){};
-
-
-var fireTurret1 = function(theply, fff153vvb){
-    if(theply.controls.fireready){if(fff153vvb){
-      for(var a=theply.turret1.numshots;a>0;a--){
-        bullets.push(new bullet(
-            theply.x-theply.turret1.shaftlength*(Math.sin(theply.rt)),
-            theply.y-theply.turret1.shaftlength*(Math.cos(theply.rt)),
-            theply.turret1.bulletspeed,
-            (((theply.rt+(Math.random()-0.5)*theply.turret1.accuracy)
-              %(2*Math.PI))+(2*Math.PI))%(2*Math.PI),
-            100,10
-        ));
-        theply.hull.t--;
+      ply.updateControls = function(){
+          
       }
-      theply.controls.fireready = false;
-    }}
-    else if(theply.controls.firetime++>theply.turret1.reloadtime)
-      {theply.controls.fireready = true;theply.controls.firetime=0;}
-};
-var fireTurret2 = function(theply, fff153vvb){
-};
+      ply.update = function(){}{
+      }
+      ply.turret1={
+          numshots:5,
+          accuracy:0.6,
+          shaftlength:64,
+          reloadtime:0,
+          bulletspeed:6
+      };
+      ply.turret2={
+          //add stuff later, defence weapon............................................................ 
+      };
+      ply.turret1.fire = function(fff153vvb){
+          if(ply.controls.fireready){if(fff153vvb){
+            for(var a=ply.turret1.numshots;a>0;a--){
+              bullets.push(new bullet(
+                  ply.x-ply.turret1.shaftlength*(Math.sin(ply.rt)),
+                  ply.y-ply.turret1.shaftlength*(Math.cos(ply.rt)),
+                  ply.turret1.bulletspeed,
+                  (((ply.rt+(Math.random()-0.5)*ply.turret1.accuracy)
+                    %(2*Math.PI))+(2*Math.PI))%(2*Math.PI),
+                  100,10
+              ));
+              ply.hull.t--;
+            }
+            ply.controls.fireready = false;
+          }}
+          else if(ply.controls.firetime++>ply.turret1.reloadtime)
+            {ply.controls.fireready = true;ply.controls.firetime=0;}              
+      };
+      ply.turret2.fire = function(fff153vvb){};
+
+
+
 var diePlayer = function(theply){
 };
 var drawPlayer = function(theplayer){
@@ -91,10 +93,10 @@ var updateCanDieYetPlayer = function(theply){
     }
 
     //control with mouse
-    if(true){fireTurret1(theply,theply.controls.clm);}
-    if(true){fireTurret2(theply,theply.controls.crm);}
+    if(true){theply.turret1.fire(theply.controls.clm);}
+    if(true){theply.turret2.fire(theply.controls.clm);}
     //players should be allowed to turn as fast as they want - NOTE: make a slider for rspeed
-    theply.rt-=theply.rspeed*0.1*theply.controls.mousehm;
+    theply.rt-=theply.controls.rspeed*0.1*theply.controls.mousehm;
     theply.controls.mousehm = 0;
   
     if(tttvx*tttvx+tttvy*tttvy-theply.speed*theply.speed>0.005){tttvx*=0.707106781;tttvy*=0.707106781;}
