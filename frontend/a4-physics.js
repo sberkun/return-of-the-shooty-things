26324c249ab07de89a11338ff706be2730aaa9fe
rsambouncer <rsambouncer@gmail.com>
2018-01-26 11:24:39 -0800

    Update a4-physics.js

--- frontend/a4-physics.js
+++ frontend/a4-physics.js
@@ -3,4 +3,16 @@
 //Bullets are fast moving objects - therefore, their hitbox is a line between their previous location and thier current location
 //people and objects are slow-moving objects. People have circular hitboxes, objects have square hitboxes.
 
-//
+var updateCanDieYetBullet = function(thebullet){
+    if(thebullet.t--<0){return true;}
+    thebullet.x+=thebullet.vx;
+    thebullet.y+=thebullet.vy;
+    return false;
+};
+
+var updateCanDieYetPlayer = function(theply){
+    if(theply.hull.t<0) return true;
+    if(theply.hull.t<theply.hull.st) theply.hull.t+=theply.hull.regen;
+    theply.x+=theply.vx;
+    theply.y+=theply.vy;   
+};
