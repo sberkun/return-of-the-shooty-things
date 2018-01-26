37c8e8eb831bc72b602d71d36ebda84b058d8139
rsambouncer <rsambouncer@gmail.com>
2018-01-25 21:07:19 -0800

    Update a4-physics.js

--- frontend/a4-physics.js
+++ frontend/a4-physics.js
@@ -1,1 +1,6 @@
+//PHYSICS SCHEME
 
+//Bullets are fast moving objects - therefore, their hitbox is a line between their previous location and thier current location
+//people and objects are slow-moving objects. People have circular hitboxes, objects have square hitboxes.
+
+//
