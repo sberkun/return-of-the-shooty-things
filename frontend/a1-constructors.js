599f74add24d7d908bc1daab781aa881edb640d3
rsambouncer <rsambouncer@gmail.com>
2018-01-29 11:27:13 -0800

    Update a1-constructors.js

--- frontend/a1-constructors.js
+++ frontend/a1-constructors.js
@@ -13,6 +13,7 @@ var RO = function(x,y,w,h){
     this.cx = this.x+this.w/2; //centerX
     this.cy = this.y+this.h/2; //centerY
     this.bubbleR = Math.sqrt(this.w*this.w+this.h*this.h)/2;
+    objects.push(this);
 };
 var drawRO = function(thero){
     DRAW.lineWidth = 3;
@@ -29,6 +30,7 @@ var bullet = function(x,y,s,r,t,d){
     this.d = d;//diameter
     this.vx = 0-this.s*Math.sin(this.r);
     this.vy = 0-this.s*Math.cos(this.r);
+    bullets.push(this);
 };
 
 var drawBullet = function(thebullet){
@@ -43,8 +45,8 @@ var drawBullet = function(thebullet){
   objects.splice(4,1);
   var wt = 20;
   var ws = 1000;
-  objects.push(new RO(-ws-wt,-ws-wt,wt,2*ws+wt));
-  objects.push(new RO(-ws-wt,ws,2*ws+wt,wt));
-  objects.push(new RO(ws,-ws,wt,2*ws+wt));
-  objects.push(new RO(-ws,-ws-wt,2*ws+wt,wt));
+  new RO(-ws-wt,-ws-wt,wt,2*ws+wt);
+  new RO(-ws-wt,ws,2*ws+wt,wt);
+  new RO(ws,-ws,wt,2*ws+wt);
+  new RO(-ws,-ws-wt,2*ws+wt,wt);
 })();
