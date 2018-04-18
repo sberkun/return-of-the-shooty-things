645c95e273a1fba7a96e789cbc698dd2e744606a
rsambouncer <rsambouncer@gmail.com>
2018-04-17 21:06:55 -0700

    Update a6-start.js

--- frontend/a6-start.js
+++ frontend/a6-start.js
@@ -4,12 +4,16 @@ var UPDATEALL = function() {
     ply.update();
     //implementServerUpdate();
         
+    alertMe("sd");
+        
     for(var p in peoples){
         if(updateCanDieYetPlayer(peoples[p])){
             peoples.splice(p,1);
             p--;
         }
     }
+        
+    alertMe("afsg");
     for(var b in bullets){
         if(updateCanDieYetBullet(bullets[b])){
             bullets.splice(b,1);
